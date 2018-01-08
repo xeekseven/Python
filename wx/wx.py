b0843f464a265dfb2356a868b392305f540ce094
xeekseven <656651296@qq.com>
2018-01-08 17:55:53 +0800

    wx.py

--- wx/wx.py
+++ wx/wx.py
@@ -1,19 +1,37 @@
 import itchat
-itchat.login()
 
-itchat.send(u'你好','filehelper')
-friends = itchat.get_friends(update=True)[0:]
-male = female =other= 0
+itchat.auto_login(True)
 
-for i in friends[1:]:
-    sex = i["Sex"]
-    if sex==1:
-        male+=1
-    elif sex==2:
-        female+=1
-    else:
-        other+=1
-total = len(friends[1:])
-print(u"男性好友：%.2f%%" % (float(male) / total * 100))
-print(u"女性好友：%.2f%%" % (float(female) / total * 100))
-print(u"其他：%.2f%%" % (float(other) / total * 100))
+friends = itchat.search_friends(name='此如犹树')
+
+itchat.send_file('wx_word.py',toUserName=friends[0]['UserName'])
+
+#消息注册 @itchat.register(TEXT,isFriendChat=True,isGroupChat=True,isMpChat=True)
+
+#文字 itchat.content.TEXT
+#图片 itchat.content.PICTURE
+#语音 itchat.content.RECORDING
+#名片 itchat.content.CARD
+
+#登录 itchat.auto_login(True)
+#登出 itchat.logout()
+
+#获取所有的朋友 itchat.get_friends()
+#获取所有的群聊 itchat.get_chatrooms()
+#获取所有的公众号 itchat.get_mps()
+
+#搜索功能
+#搜索朋友 itchat.search_friends(self,name=None,userName=None,nickName=None)
+#搜索公众号 itchat.search_mps(self,name=None,userName=None)
+#搜索群聊 itchat.search_chatrooms(self,name=None,userName=None)
+
+
+
+
+#UserName是指的一个标识。不仅用户有，群聊也有，公众号也有，所以这一句可以给三种'用户'发送消息。
+#发送消息 itchat.send(message,UserName)
+
+#发送消息 send_msg  itchat.send_msg("hi",toUserName=None)
+#发送文件 send_file itchat.send_file(file_path,toUserName=None)
+#发送图片 send_img itchat.send_img(img_path,toUserName=None)
+#发送流媒体 send_video itchat.send_video(video_path,toUserName=None)
